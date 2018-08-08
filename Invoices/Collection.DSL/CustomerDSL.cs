31baaead5c7f5ac468429b91ec37b0abf0eab046
HebaAhmed <eng.hebaahmed94@gmail.com>
2018-08-08 14:19:33 +0200

    882

--- Invoices/Collection.DSL/CustomerDSL.cs
+++ Invoices/Collection.DSL/CustomerDSL.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using Collection.Repository;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Collection.DAL;
+
 
 namespace Collection.DSL
 {
-    public class Class1
+    public class CustomerDSL
     {
+        CustomerRepo repo = new CustomerRepo();
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            var list = repo.GetCustomers();
+            return list;
+
+        }
+        public Customer GetCustomerByByID(int id)
+
+        {
+            var customer = repo.GetCustomerByID(id);
+            return customer;
+        }
+
+        public void InsertCustomer(Customer customer)
+        {
+            repo.InsertCustomer(customer);
+        }
+        public void DeleteCustomer(int id)
+        {
+            repo.DeleteCustomer(id);
+        }
+        public void UpdateCustomer(Customer customer)
+        {
+            repo.UpdateCustomer(customer);
+        }
+        public void CommitCustomer()
+        {
+            repo.CommitCustomer();
+        }
     }
 }
--- Invoices/Collection.Repository/CustomerRepo.cs
+++ Invoices/Collection.Repository/CustomerRepo.cs
@@ -3,10 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data;
+using Collection.DAL;
+
 
 namespace Collection.Repository
 {
-    public class Class1
+
+    public class CustomerRepo
     {
+         private InvoicesEntities context;
+        
+
+
+        public CustomerRepo()
+        {
+            context = new InvoicesEntities ();
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return context.Customers.ToList();
+        }
+
+        public Customer GetCustomerByID(int id)
+        {
+            return context.Customers.Find(id);
+        }
+
+        public void InsertCustomer(Customer customer)
+        {
+            context.Customers.Add(customer);
+        }
+
+        public void DeleteCustomer(int CustomerId)
+        {
+            Customer customer = context.Customers.Find(CustomerId);
+            context.Customers.Remove(customer);
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            context.Entry(customer).State = EntityState.Modified;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        //    private bool disposed = false;
+
+        //    protected virtual void Dispose(bool disposing)
+        //    {
+        //        if (!this.disposed)
+        //        {
+        //            if (disposing)
+        //            {
+        //                context.Dispose();
+        //            }
+        //        }
+        //        this.disposed = true;
+        //    }
+
+        //    public void Dispose()
+        //    {
+        //        Dispose(true);
+        //        GC.SuppressFinalize(this);
+        //    }
+        //}
     }
 }
+
