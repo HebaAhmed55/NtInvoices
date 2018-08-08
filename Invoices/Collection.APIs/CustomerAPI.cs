9231e266091af9da2bf959582f5b36ed661f690a
HebaAhmed <eng.hebaahmed94@gmail.com>
2018-08-08 15:06:36 +0200

    hk

--- Invoices/Collection.APIs/CustomerAPI.cs
+++ Invoices/Collection.APIs/CustomerAPI.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Collection.DSL;
+using System.Net.Http;
 
 namespace Collection.APIs
 {
     public class CustomerAPI : ApiController
     {
-       
+        CustomerDSL c = new CustomerDSL();
+        //public HttpResponseMessage Post(Customer customer)
+        //{
+        //    HttpResponseMessage result;
+        //    if (ModelState.IsValid)
+        //    {
+        //        c.InsertCustomer(customer);
+        //        c.CommitCustomer();
+        //       return  this.Request.CreateResponse(HttpStatusCode.OK);
+        //    }
+        //    else
+        //    {
+        //        result = Request.CreateResponse(HttpStatusCode.BadRequest, "Server failed to save data");
+        //    }
+        //    return result;
+        //}
     }
 }
