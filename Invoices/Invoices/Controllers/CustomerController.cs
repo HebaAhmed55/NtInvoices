ea1ae2383fbb7fe68346a6ec965421f7ade4339e
HebaAhmed <eng.hebaahmed94@gmail.com>
2018-08-08 16:49:15 +0200

    kjlkj

--- Invoices/Invoices/Controllers/CustomerController.cs
+++ Invoices/Invoices/Controllers/CustomerController.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Collection.DSL;
+using Collection.DAL;
 
 namespace Invoices.Controllers
 {
     public class CustomerController : Controller
     {
+        CustomerDSL c = new CustomerDSL();
         // GET: Customer
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Setup()
+        public ActionResult Create()
         {
             return View();
         }
